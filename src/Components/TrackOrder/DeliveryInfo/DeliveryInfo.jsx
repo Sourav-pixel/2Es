d2c7e24e2e6b6bb5bee11597b1aaf37c08e7c2d1
Sourav Deka <84978182+Sourav-pixel@users.noreply.github.com>
2023-09-22 03:21:39 +0530

    major commit

--- src/Components/TrackOrder/DeliveryInfo/DeliveryInfo.jsx
+++ src/Components/TrackOrder/DeliveryInfo/DeliveryInfo.jsx
@@ -2,9 +2,10 @@ import { } from 'react';
 import delivery_man from "../../../assets/images/delivery_icons/delivery_man.png";
 import { useNavigate } from 'react-router-dom';
 import { Button, Step, StepButton, Stepper } from '@mui/material';
-import { localStorageHandler } from '../../../assets/FakeData/FakeData';
+// import { localStorageHandler } from '../../../assets/FakeData/FakeData';
 
-
+import Lottie from 'react-lottie';
+import successAnimationData from './success.json';
 const DeliveryInfo = ({ delivery }) => {
     const navigate = useNavigate();
     const handleWhatsAppClick = () => {
@@ -24,64 +25,44 @@ const handleClick = () => {
   
 }
 
+const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: successAnimationData, // The animation data you imported
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
     return (
         <div className='col bg-gray-100 lg:order-none order-first space-y-5 px-4 lg:py-6 py-5 rounded-xl mx-auto w-full sm:max-w-[30rem] max-w-[26rem] lg:max-w-[21rem]'>
-            <div className='space-y-2.5'>
-                {/* Delivery_man Img */}
-                <div className='pl-5'>
-                    <img className='max-h-20' src={delivery_man} alt="delivery-man" />
-                </div>
-                {/* Locations Info */}
-                <div className='p-3.5 bg-white rounded-lg'>
-                    <div className="space-y-4">
-
-                        {[{ id: 0, title: 'Your Location', location: delivery.customer_location },
-                        { id: 1, title: 'Shop Location', location: delivery.restaurant_location }].map(step => (
-
-                            <div key={step.id} className="flex space-x-2.5 items-center">
-                                <div className="w-2 h-2 bg-red-600 rounded-full"></div>
-                                <div className="mt-auto space-y-">
-                                    <h5 className='font-sans text-sm font-semibold'>{step.title}</h5>
-                                    <p className='text-xs text-gray-500'>{step.location}</p>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-
-            <div className='space-y-4'>
-                {/* Estimated Time */}
-            
-            </div>
-
-            {/* Contact */}
-            <div>
-          
-              
-                    <Button fullWidth
-                        color='success'
-                        variant='contained'
-                        onClick={handleWhatsAppClick}
-                        >
-                        Contact
-                    </Button>
-              
-            </div>
-            <div>
-          
-  
-              <Button fullWidth
-                  color='error'
-                  variant='contained'
-                  onClick={handleClick}
-                  >
-                  Cancel Order
-              </Button>
-        
+      <div className='space-y-2.5'></div>
+      <div className='space-y-0'>
+      <Lottie options={defaultOptions} height={200} width={200} />
       </div>
-
-        </div>
+      {/* Contact */}
+      <div>
+        <Button
+          fullWidth
+          color='success'
+          variant='contained'
+          onClick={handleWhatsAppClick}
+        >
+          Contact
+        </Button>
+      </div>
+      <div>
+  
+        <Button
+          fullWidth
+          color='error'
+          variant='contained'
+          onClick={handleClick}
+        >
+          Cancel Order
+        </Button>
+      </div>
+    </div>
     );
 };
 
